9bb67b64165a25096eb2b8157fd9529a54ab5306
ayush <ayushkhobragade17@gmail.com>
2023-02-27 18:29:11 +0530

    added Form Validations

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const LoginWrapper = styled.div`
@@ -37,22 +38,72 @@ const FormGroup = styled.form`
     font-size: 15px;
     border: none;
   }
+  button:hover {
+    background-color: #000;
+    border: 1px solid #fff;
+  }
 `;
 function Login() {
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+  const [userErr, setUserErr] = useState("");
+  const [passErr, setPassErr] = useState("");
+
+  const onChangeUserHandler = (e) => {
+    const value = e.target.value;
+
+    setUser(value);
+    console.log(setUser);
+    setUserErr("");
+  };
+  const onChangePassHandler = (e) => {
+    const value = e.target.value;
+    setPass(value);
+    console.log(setPass);
+    setPassErr("");
+  };
+  const onSubmitHandler = (e) => {
+    const isUser = user.trim() === "";
+    const isPass = pass.trim() === "";
+    if (isUser) {
+      setUserErr("Please enter Your Username");
+    }
+    if (isPass) {
+      setPassErr("please Enter Your Password");
+    }
+    e.preventDefault();
+  };
   return (
     <LoginWrapper>
       <header>
         <h2>Login</h2>
       </header>
-      <main>
-        <FormGroup>
-          <label>Username</label>
-          <input type="text" placeholder="username" />
-          <label>Password</label>
-          <input type="password" placeholder="password" />
-          <button>Login</button>
-        </FormGroup>
-      </main>
+
+      <FormGroup onSubmit={onSubmitHandler}>
+        <label For="username">Username</label>
+        <input
+          type="text"
+          placeholder="username"
+          value={user}
+          id="username"
+          onChange={onChangeUserHandler}
+        />
+        {userErr.length > 0 && (
+          <span style={{ color: "#EB455F", fontSize: 10 }}>{userErr}</span>
+        )}
+        <label for="password">Password</label>
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          value={pass}
+          onChange={onChangePassHandler}
+        />
+        {passErr.length > 0 && (
+          <span style={{ color: "#EB455F", fontSize: 10 }}>{passErr}</span>
+        )}
+        <button>Login</button>
+      </FormGroup>
     </LoginWrapper>
   );
 }
